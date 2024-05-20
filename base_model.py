b70e0f328dc10f6ddec01723c553b4ec72e8625f
hillary <hillary.hauger@tum.de>
2024-05-20 10:58:42 +0200

    Added class for base model such that we have a unified code

---
+++ base_model.py
@@ -0,0 +1,35 @@
+
+import torch.nn as nn
+
+class base_model(nn.Module):
+    @abstractmethod
+    def decode(self, z):
+        """
+        Takes the latent code as input and returns the reconstructed image.
+        """
+        pass
+
+    @abstractmethod    
+    def encode(self, x):
+        """"
+        Encodes the input by passing through the encoder network
+        """
+        pass
+
+
+    @abstractmethod
+    def forward(self, x):
+        """
+        Passes through the encoder and decoder and returns the output.
+        Using reparameterization trick to sample from the latent space.
+        """
+        pass
+    
+    
+
+    @abstractmethod
+    def getLatenSpace(self, x):
+        """
+        Returns the latent space representation of the input. Last Layer of the Encoder before the mean and variance.
+        """
+        pass
