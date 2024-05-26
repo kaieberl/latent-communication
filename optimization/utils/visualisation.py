f2fff8e359dce05661bb3d6360157c2ee5d3973b
hillary <hillary.hauger@tum.de>
2024-05-26 19:09:03 +0200

    Ran optimization for ResNet model and started with visualisation

---
+++ optimization/utils/visualisation.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+from sklearn.decomposition import PCA
+
+
+def pca_def(v):
+    pca = PCA(n_components=2)
+    pca.fit(v)
+    return pca
+
+def get_latent_space_data(model, data_loader, DEVICE,N=1000):
+    images, _ = next(iter(data_loader.train_loader))
+    latent_spaces = []
+    all_labels = []
+
+    for images, labels in data_loader.train_loader:
+        #Exit for loop if all labels is as long as N
+        if len(all_labels) > N:
+            break
+        images = images.to(DEVICE)
+        #images = images.view(images.size(0), -1)
+        latent_space = model.get_latent_space(images)
+        latent_space = latent_space.cpu().detach().numpy()
+        latent_spaces.append(latent_space)
+        all_labels.append(labels.numpy())
+
+    # Concatenate latent space representations from all batches
+    latent_space = np.concatenate(latent_spaces, axis=0)
+    all_labels = np.concatenate(all_labels, axis=0)
+
+    return latent_space, all_labels
+
+
+def plotLatentTransformed(latent_space,all_labels, A, pca1, name):
+    # Transform the latent space
+    latent_space_transformed = np.dot(latent_space, A.T)
+    # Plot latent space via PCA
+    latent_space_pca = pca1.transform(latent_space_transformed)
+    # Plot the latent space
+    plot = plt.scatter(latent_space_pca[:, 0], latent_space_pca[:, 1], c=all_labels, cmap='tab10', label=all_labels)
+    plt.title('Latent Space '+ name)
+    plt.show(plot)
+    return  plot
+
+def avg_transformed_distances(z1, z2, A):
+    # Compute the transformed distances
+    distance_avg = 0
+    for i in range(z1.shape[0]):
+        distance_avg += np.linalg.norm(A @ z1[i] - z2[i])
+    distance_avg /= z1.shape[0]
+    return distance_avg
