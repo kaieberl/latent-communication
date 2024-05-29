af5a3992bc5a33195c13d840ec3277e6032b77a1
Kai Eberl <kai.eberl@tum.de>
2024-05-29 15:21:05 +0200

    create package structure for importing submodules

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Latent Communication',
+    version='0.1.0',
+    url='https://github.com/kaieberl/latent-communication',
+    author='',
+    author_email='',
+    description='Code base for Latent Communication case studies project',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'torch',
+        'torchvision',
+        'lightning',
+        'pandas',
+        'matplotlib',
+        'scikit-learn'
+    ],
+)
