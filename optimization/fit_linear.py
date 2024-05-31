3a134667924992864f7a047d2c80533fbf93d637
Kai Eberl <kai.eberl@tum.de>
2024-05-31 12:52:52 +0200

    improve linear optimization code

--- optimization/fit_linear.py
+++ optimization/fit_linear.py
@@ -1,78 +1,50 @@
-"""Load two pre-trained models and perform linear and affine fitting between their latent spaces, or between two sets
-of latent vectors."""
-
-import os
-
+from pathlib import Path
 import torch
-
+from omegaconf import DictConfig
+import hydra
 from stitching.stitching import get_transformations, load_model
 from optimizer import AffineFitting, LinearFitting
 from utils.sampler import simple_sampler
 
-# Set device
 device = torch.device('cuda') if torch.cuda.is_available() else 'cpu'
 
-# Configuration
-base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-seed1 = 0
-seed2 = 1
 
-config = {
-    'model1': {
-        # 'path': os.path.join(base_path, f"vit/models/vit_mnist_seed{seed1}.pth"),
-        'label_path': os.path.join(base_path, "vit/models/labels_train.pth"),
-        'latents_path': os.path.join(base_path, f"vit/models/latent_space_vit_seed{seed1}_train.pth"),
-        'name': 'vit',
-        'seed': seed1
-    },
-    'model2': {
-        # 'path': os.path.join(base_path, f"vit/models/vit_mnist_seed{seed2}.pth"),
-        'latents_path': os.path.join(base_path, f"vit/models/latent_space_vit_seed{seed2}_train.pth"),
-        'name': 'vit',
-        'seed': seed2
-    }
-}
-
-
-def get_latents(config):
+def get_latents(cfg):
     """For both models, load the latent vectors if latent_path is provided, else load the models and sample the
-    latent vectors."""
-
+        latent vectors."""
     torch.manual_seed(0)
-    indices = torch.randperm(60000)[:1000]
-    if 'latents_path' in config:
-        labels = torch.load(config['label_path'], map_location=device)
-        latents1 = torch.load(config['latents_path'], map_location=device)
-        latents2 = torch.load(config['latents_path'], map_location=device)
-        latents1 = latents1[indices]
-        latents2 = latents2[indices]
-    else:
-        model1 = load_model(config['modelname1'], config['path1'])
-        model2 = load_model(config['modelname2'], config['path2'])
+    indices = torch.randperm(60000)[:cfg.num_samples]
+    latents = {}
+    for model_name in ['model1', 'model2']:
+        if 'latents_path' in cfg[model_name]:
+            labels = torch.load(cfg[model_name].label_path, map_location=device)
+            z = torch.load(cfg[model_name].latents_path, map_location=device)
+            latents[model_name] = z[indices]
+        else:
+            model = load_model(cfg[model_name].name, cfg[model_name].path)
+            transforms = get_transformations(cfg[model_name].name)
+            latents[model_name] = simple_sampler(indices, model, transforms, device, seed=cfg[model_name].seed)
 
-        transforms1 = get_transformations(config['modelname1'])
-        transforms2 = get_transformations(config['modelname2'])
-
-        latents1 = simple_sampler(indices, model1, transforms1, device, seed=seed1)
-        latents2 = simple_sampler(indices, model2, transforms2, device, seed=seed2)
-
-    return latents1, latents2
+    return latents
 
 
-def main():
-    latents1, latents2 = get_latents(config)
+@hydra.main(config_path="config", config_name="config_resnet")
+def main(cfg : DictConfig) -> None:
+    cfg.base_dir = Path(hydra.utils.get_original_cwd()).parent
+    latents1, latents2 = get_latents(cfg).values()
 
     # Linear transform
     linear_fitting = LinearFitting(latents1, latents2, lamda=0.01)
     linear_fitting.solve_problem()
-    linear_fitting.print_results()
-    linear_fitting.save_results("results/linear_fitting.npz")
+    storage_path = Path(cfg.storage_path) / f"Linear_{cfg.model1.name}_{cfg.model1.seed}_{cfg.model2.name}_{cfg.model2.seed}_{cfg.num_samples}"
+    linear_fitting.save_results(storage_path)
+    del linear_fitting
 
     # Affine transform
     affine_fitting = AffineFitting(latents1, latents2, lamda=0.01)
     affine_fitting.solve_problem()
-    affine_fitting.print_results()
-    affine_fitting.save_results("results/affine_fitting.npz")
+    storage_path = Path(cfg.storage_path) / f"Affine_{cfg.model1.name}_{cfg.model1.seed}_{cfg.model2.name}_{cfg.model2.seed}_{cfg.num_samples}"
+    affine_fitting.save_results(storage_path)
 
 
 if __name__ == '__main__':
