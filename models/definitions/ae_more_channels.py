e1bc66c994b0c6f020e1a7c090da5918b9116347
deadlyseagull <45318100+federicoferoggio@users.noreply.github.com>
2024-06-03 00:08:43 +0300

    Trained all models for the AE. Chose in definitive the datasets shown as they are the ones for which the paper's training is available. Look also into the Full_train_setup file to quickly train over possible condigurations.

---
+++ models/definitions/ae_more_channels.py
@@ -0,0 +1,117 @@
+import torch
+from models.definitions.base_model import LightningBaseModel
+
+import torch.nn.init as init
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+class LightningAutoencoderV2(LightningBaseModel):
+    def __init__(self):
+        super().__init__()
+        self.autoencoder = nn.Module()
+        self.autoencoder.encoder = nn.ModuleList([
+        nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(32),
+            nn.GELU(),),
+        nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.GELU()),
+        nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.GELU()),
+        nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.GELU(),
+        )])
+        self.autoencoder.decoder = nn.ModuleList([
+            nn.Sequential(
+                nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),
+                nn.BatchNorm2d(128),
+                nn.GELU()),
+            nn.Sequential(
+                nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
+                nn.BatchNorm2d(64),
+                nn.GELU()),
+            nn.Sequential(
+                nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
+                nn.BatchNorm2d(32),
+                nn.GELU()),
+            nn.Sequential(
+                nn.ConvTranspose2d(32, 32, kernel_size=4, stride=2, padding=1),
+                nn.BatchNorm2d(32),
+                nn.GELU()),
+            nn.Sequential(
+                nn.Conv2d(32, 3, kernel_size=3, stride=1, padding=1),
+                nn.Sigmoid()),
+        ])
+        self.autoencoder.encoder_out = nn.Sequential(
+            nn.Linear(1024, 500),
+            nn.GELU(),
+        )
+        self.autoencoder.decoder_in = nn.Sequential(
+            nn.Linear(500, 1024),
+            nn.GELU(),
+        )
+        self.hidden_dim = 500
+
+    def encode(self, x):
+        for layer in self.autoencoder.encoder:
+            x = layer(x)
+        x = x.view(x.size(0), -1)
+        x = self.autoencoder.encoder_out(x)
+        return x
+
+    def decode(self, x):
+        x = self.autoencoder.decoder_in(x)
+        x = x.view(x.size(0), 256, 2, 2)
+        for layer in self.autoencoder.decoder:
+            x = layer(x)
+        return x
+
+    def forward(self, x):
+        for layer in self.autoencoder.encoder:
+            x = layer(x)
+        x = x.view(x.size(0), -1)
+        x = self.autoencoder.encoder_out(x)
+        x = self.autoencoder.decoder_in(x)
+        x = x.view(x.size(0), 256, 2, 2)
+        for layer in self.autoencoder.decoder:
+            x = layer(x)
+        return x
+
+    def init_weights(self, m):
+        if type(m) == nn.Linear:
+            init.kaiming_normal_(m.weight)
+            m.bias.data.fill_(0.01)
+    
+
+    def training_step(self, batch):
+        x = batch
+        x_hat = self.forward(x)
+        loss = F.mse_loss(x_hat, x)
+        self.log('train_loss', loss)
+        return loss
+
+    def validation_step(self, batch):
+        x = batch
+        x_hat = self.forward(x)
+        loss = F.mse_loss(x_hat, x)
+        self.log('val_loss', loss)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+    
+    def get_latent_space(self, x):
+        for layer in self.autoencoder.encoder:
+            x = layer(x)
+        x = x.view(x.size(0), -1)
+        x = self.autoencoder.encoder_out(x)
+        return x
---
+++ utils/dataloaders/dataloader_cifrar100_single.py
@@ -0,0 +1,37 @@
+import os
+
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from torch.utils.data import Subset
+import numpy as np
+
+
+class DataLoaderCIFAR100:
+    def __init__(self, batch_size, transformation, seed=0, indices=None):
+        self.indices = indices
+        self.seed = seed
+        self.batch_size = batch_size
+        transform = transforms.Compose(transformation)
+
+        dataset_class = datasets.CIFAR100
+        base_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+        train = dataset_class(root=base_dir + '/data', train=True, download=True, transform=transform)
+        test = dataset_class(root=base_dir + '/data', train=False, download=True, transform=transform)
+
+        np.random.seed(self.seed)
+
+        # Create Subsets with the same order of indices
+        if self.indices is None:
+            self.indices = np.random.permutation(len(train))
+        subset = Subset(train, self.indices)
+
+        # Create DataLoaders
+        self.train_loader = DataLoader(subset, batch_size=batch_size, shuffle=False, num_workers=0)
+        self.test_loader = DataLoader(test, batch_size=batch_size, shuffle=False, num_workers=0)
+
+    def get_train_loader(self):
+        return self.train_loader
+
+    def get_test_loader(self):
+        return self.test_loader
