002e47017e0e6ae9f6f7e4252eabcefa3d9e47fa
Kai Eberl <kai.eberl@tum.de>
2024-06-06 21:16:43 +0200

    bug fixes

--- models/train.py
+++ models/train.py
@@ -1,21 +1,25 @@
+import os.path
+from pathlib import Path
+
 import hydra
 import torch
 import torch.utils.data
 import lightning as L
 from lightning import Trainer
 from torchvision.datasets import MNIST
+from torchvision.transforms import Compose
 
 from utils.model import get_transformations, load_model
 
 
 class MNISTDataModule(L.LightningDataModule):
-    def __init__(self, data_dir="../data", batch_size=64):
+    def __init__(self, transformations, data_dir=Path(os.path.realpath(__file__)).parent.parent / "data", batch_size=64):
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
 
         # Define transformations (same as before)
-        self.transform = get_transformations('resnet_ae')
+        self.transform = transformations
 
     def setup(self, stage=None):
         MNIST(root=self.data_dir, train=stage == "fit", download=True, transform=self.transform)
@@ -36,12 +40,13 @@ def main(cfg):
     for seed in seeds:
         L.seed_everything(seed)
 
-        data_module = MNISTDataModule()
-        model = load_model(cfg.model.name)
+        transformations = Compose(get_transformations(cfg.name))
+        data_module = MNISTDataModule(transformations)
+        model = load_model(cfg.name)
         trainer = Trainer(max_epochs=10)
         trainer.fit(model, datamodule=data_module)
 
-        torch.save(model.state_dict(), cfg.model.path)
+        torch.save(model.state_dict(), cfg.path)
 
 
 if __name__ == '__main__':
