3d92f369336f038af8c54b7994c368de00fcc5c2
hillary <hillary.hauger@tum.de>
2024-07-08 12:22:40 +0200

    Added decouple to get_mapping and tried to improve the decoupling method

--- utils/get_mapping.py
+++ utils/get_mapping.py
@@ -8,7 +8,7 @@ def load_mapping(path,mapping):
     elif mapping.lower() == 'neuralnetwork':
         from optimization.optimizer import NeuralNetworkFitting
         mapping = NeuralNetworkFitting.from_file(path)
-    elif mapping == 'decouple':
+    elif mapping.lower() == 'decouple':
         from optimization.optimizer import DecoupleFitting
         mapping = DecoupleFitting.from_file(path)
     else:
